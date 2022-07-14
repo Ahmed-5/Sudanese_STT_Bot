332e46da3857e0da5c7facfc771b997cd69a1640
ahmed-5 <ahmedelhassen11@gmail.com>
2022-07-14 21:29:30 +0200

    voice messages handling

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 const { Telegraf } = require("telegraf");
+
+const helpMessage = `
+This is the help message
+/start to start the bot
+/help to show how to use the bot
+`;
+
+let message = "";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -11,12 +19,28 @@ bot.command("quit", (ctx) => {
   ctx.leaveChat();
 });
 
+bot.start((ctx) => {
+  ctx.reply("Hi I am Sudanese Speech To Text Bot");
+  ctx.reply(helpMessage);
+});
+
+bot.help((ctx) => {
+  ctx.reply(helpMessage);
+});
+
 bot.on("text", (ctx) => {
   // Explicit usage
-//   ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
+  //   ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
 
   // Using context shortcut
+  message = ctx.message.text;
+  bot.telegram.sendMessage("-721517271", ctx.message.text);
   ctx.reply(`Hello @${ctx.message.from.username}.`);
+});
+
+bot.on("voice", (ctx) => {
+  if (message) bot.telegram.sendMessage("-721517271", message); // suppose to be the labels
+  bot.telegram.sendVoice("-721517271", ctx.message.voice.file_id);
 });
 
 bot.launch();
