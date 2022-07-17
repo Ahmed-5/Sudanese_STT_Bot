82c2be3fa904c035d802604277e9bbb74f700040
ahmed-5 <ahmedelhassen11@gmail.com>
2022-07-18 00:48:09 +0200

    minimum viable product

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
+const { google } = require("googleapis");
+
+const spreadsheetId = "1TzEeyni-DrH2gjiXZtgpDJR18-oCm2vnQloNtc4cBE8";
 
 const helpMessage = `
 This is the help message
@@ -7,44 +10,137 @@ This is the help message
 /help to show how to use the bot
 `;
 
-let message = "";
+const creds = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+};
+
+const auth = new google.auth.GoogleAuth({
+  credentials: creds,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+const initGoogle = async () => {
+  const client = await auth.getClient();
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+  return { client, googleSheets };
+};
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command("quit", (ctx) => {
-  // Explicit usage
-  ctx.telegram.leaveChat(ctx.message.chat.id);
+initGoogle()
+  .then(async ({ client, googleSheets }) => {
+    let message = "";
+    let index = 0;
+    let length = 0;
 
-  // Using context shortcut
-  ctx.leaveChat();
-});
+    let getRows = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "Sheet1!A:A",
+    });
 
-bot.start((ctx) => {
-  ctx.reply("Hi I am Sudanese Speech To Text Bot");
-  ctx.reply(helpMessage);
-});
+    // console.log("ready");
+    // console.log(getRows.data);
 
-bot.help((ctx) => {
-  ctx.reply(helpMessage);
-});
+    bot.start((ctx) => {
+      ctx.reply("Hi I am Sudanese Speech To Text Bot");
+      ctx.reply(helpMessage, {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Start Recording", callback_data: "record" }],
+          ],
+        },
+      });
+    });
 
-bot.on("text", (ctx) => {
-  // Explicit usage
-  //   ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`);
+    bot.help((ctx) => {
+      ctx.reply(helpMessage);
+    });
 
-  // Using context shortcut
-  message = ctx.message.text;
-  bot.telegram.sendMessage("-721517271", ctx.message.text);
-  ctx.reply(`Hello @${ctx.message.from.username}.`);
-});
+    bot.on("text", (ctx) => {
+      ctx.reply(`Hello @${ctx.message.from.username}.`);
+    });
 
-bot.on("voice", (ctx) => {
-  if (message) bot.telegram.sendMessage("-721517271", message); // suppose to be the labels
-  bot.telegram.sendVoice("-721517271", ctx.message.voice.file_id);
-});
+    bot.on("voice", (ctx) => {
+      if (message) bot.telegram.sendMessage("-721517271", message); // suppose to be the labels
+      bot.telegram.sendVoice("-721517271", ctx.message.voice.file_id);
+    });
 
-bot.launch();
+    bot.action("record", async (ctx) => {
+      getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "Sheet1!A:A",
+      });
+      length = getRows.data.values.length;
+      index = 0;
+      if (getRows.data.values[0] !== undefined) {
+        ctx.deleteMessage();
+        message = getRows.data.values[0][0];
+        ctx.reply(message, {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                { text: "prev", callback_data: "prev" },
+                { text: "next", callback_data: "next" },
+              ],
+              [{ text: "refresh", callback_data: "record" }],
+            ],
+          },
+        });
+      }
+    });
 
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+    bot.action("next", async (ctx) => {
+      ctx.deleteMessage();
+      index = (index - 1 + length) % length;
+      message = getRows.data.values[index][0];
+      ctx.reply(message, {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: "prev", callback_data: "prev" },
+              { text: "next", callback_data: "next" },
+            ],
+            [{ text: "refresh", callback_data: "refresh" }],
+          ],
+        },
+      });
+    });
+
+    bot.action("prev", async (ctx) => {
+      ctx.deleteMessage();
+      index = (index - 1 + length) % length;
+      message = getRows.data.values[index][0];
+      ctx.reply(message, {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: "prev", callback_data: "prev" },
+              { text: "next", callback_data: "next" },
+            ],
+            [{ text: "refresh", callback_data: "record" }],
+          ],
+        },
+      });
+    });
+
+    bot.launch();
+
+    // Enable graceful stop
+    process.once("SIGINT", () => bot.stop("SIGINT"));
+    process.once("SIGTERM", () => bot.stop("SIGTERM"));
+  })
+  .catch((e) => {
+    console.log(e);
+    console.log(e.validateStatus);
+  });
